67cee9d4392d4bdbd1c57b8ef9b7767a6c0d44a5
codelirium <codeliriumblog@gmail.com>
2018-05-01 23:31:06 +0200

    preview server

--- preview-server.js
+++ preview-server.js
@@ -9,6 +9,7 @@ app.use(express.static('build'));
 app.get('/', (req, res) => res.sendFile('index.html', { root: path.resolve(__dirname, 'build') }));
 app.get('/about', (req, res) => res.sendFile('about.html', { root: path.resolve(__dirname, 'build') }));
 app.get('/888', (req, res) => res.sendFile('listpage.html', { root: path.resolve(__dirname, 'build') }));
+app.get('/article', (req, res) => res.sendFile('articlepage.html', { root: path.resolve(__dirname, 'build') }));
 app.get('/ranking', (req, res) => res.sendFile('ranking.html', { root: path.resolve(__dirname, 'build') }));
 app.get('/search', (req, res) => res.sendFile('search.html', { root: path.resolve(__dirname, 'build') }));
 
